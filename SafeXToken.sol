4f44f5c991ec1c8a4c49571a3b7f9755c8ab377a
naveenvinoy <56766859+naveenvinoy@users.noreply.github.com>
2020-09-17 23:25:23 +0530

    Update SafeXToken.sol

--- SafeXToken.sol
+++ SafeXToken.sol
@@ -1,1 +1,52 @@
 
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import "./IXToken.sol";
+import "./SafeMath.sol";
+import "./Address.sol";
+
+
+library SafeXToken {
+    using SafeMath for uint256;
+    using Address for address;
+
+    function safeTransfer(IXToken token, address to, uint256 value) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
+    }
+
+    function safeTransferFrom(IXToken token, address from, address to, uint256 value) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
+    }
+
+   
+    function safeApprove(IXToken token, address spender, uint256 value) internal {
+       
+        require((value == 0) || (token.allowance(address(this), spender) == 0),
+            "SafeERC20: approve from non-zero to non-zero allowance"
+        );
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
+    }
+
+    function safeIncreaseAllowance(IXToken token, address spender, uint256 value) internal {
+        uint256 newAllowance = token.allowance(address(this), spender).add(value);
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+    }
+
+    function safeDecreaseAllowance(IXToken token, address spender, uint256 value) internal {
+        uint256 newAllowance = token.allowance(address(this), spender).sub(value, "SafeERC20: decreased allowance below zero");
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+    }
+
+    
+    function _callOptionalReturn(IXToken token, bytes memory data) private {
+        
+
+        bytes memory returndata = address(token).functionCall(data, "SafeERC20: low-level call failed");
+        if (returndata.length > 0) { 
+          
+            require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
+        }
+    }
+}
